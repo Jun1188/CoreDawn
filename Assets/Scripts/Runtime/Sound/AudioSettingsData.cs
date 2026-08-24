ccad5f1705c66be1647a506d0507465dede591d1
Byun junyoung(변준영) <135by135by@gmail.com>
2026-08-24 20:03:26 +0900

    reduce sfx Audio setting
    reduce sfx Audio

--- Assets/Scripts/Runtime/Sound/AudioSettingsData.cs
+++ Assets/Scripts/Runtime/Sound/AudioSettingsData.cs
@@ -3,5 +3,5 @@ public class AudioSettingsData
 {
     public float masterVolume = 0.8f;
     public float bgmVolume = 0.8f;
-    public float sfxVolume = 0.8f;
+    public float sfxVolume = 0.2f;
 }
